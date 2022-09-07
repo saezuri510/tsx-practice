d7480f42c785ffb6054f6498ada1791336627ebf
saezuri510 <saezuru510@gmail.com>
2022-09-07 20:21:21 +0900

    tsx練習1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState<number>(0);
+    const add = () => {
+        setCount((prev) => prev + 1);
+    };
+    const doubleAdd = () => {
+        add();
+        add();
+    };
+
+    return (
+        <div className="App">
+            {count === 3 ? (
+                <Button onClick={() => setCount(0)}>clear!</Button>
+            ) : (
+                <p>You clicked {count} times</p>
+            )}
+            <button onClick={add}>+1</button>
+            <button onClick={() => setCount((prev) => prev - 1)}>-1</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+            <button onClick={doubleAdd}>Double Add</button>
+        </div>
+    );
 }
 
 export default App;
