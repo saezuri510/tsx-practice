edfcbe4cef5f28e09942015d821f43586609be3f
saezuri510 <saezuru510@gmail.com>
2022-09-09 17:57:43 +0900

    tsx-welcome練習6

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -1,13 +1,20 @@
 import type { DataItem } from "./DataItem";
 
-// const isUndefined = (value)=>{
+// const test = ((data: DataItem)=>{
+//     return(
+//         <div>
+//             <div style={{display: data.name !== undefined ? "block" : "none"}}>Hello, {data.name}</div>
+//         </div>
+//     )
+// });
+
+// const isUndefined = ((value: ~~)=>{
 //     return value !== undefined;
-// }
+// });
 
 export const Welcome: React.FC<DataItem> = ({ name, id, age }) => {
     return (
         <div>
-            {/* {isUndefined(name) && <div>Hello, {name}} */}
             {name !== undefined && <div>Hello, {name}</div>}
             {id !== undefined && <div>id: {id}</div>}
             {age !== undefined && <div>age: {age}</div>}
